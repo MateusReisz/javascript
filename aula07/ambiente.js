df05467f17ef6140d029cd00ff04149366d9eb9a
MateusReisz <136511001+MateusReisz@users.noreply.github.com>
2023-08-03 23:23:14 -0300

    exercicios de repeticoes

---
+++ aula07/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+/*
+----- FAZ PRIMEIRO E TESTA DEPOS -----
+
+do{
+    console.log(`Te amo mais de ${c} vezes`)
+    c++
+}while(c<5)
+*/
+
+
+/*
+----- TESTA PRIMEIRO E FAZ DEPOIS -----
+
+while (c<6){
+    console.log('Te amo!')
+    c++
+}
+*/
+
+
+/*
+FORMA BRUTA DE FAZER ALGO
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
